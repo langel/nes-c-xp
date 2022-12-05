b3970cdc6ef887df99a4482212a81befa33ea559
langel <knoxburry@gmail.com>
2022-12-05 02:29:00 -0500

    making a mess

--- main.c
+++ main.c
@@ -23,6 +23,18 @@
 // link the pattern table into CHR ROM
 //#link "chr_generic.s"
 
+#define PPU_CTRL	0x2000
+#define PPU_MASK	0x2001
+#define PPU_STATUS	0x2002
+#define OAM_ADDR	0x2003
+#define OAM_DATA	0x2004
+#define PPU_SCROLL	0x2005
+#define PPU_ADDR	0x2006
+#define PPU_DATA	0x2007
+
+#include "tables.c"
+
+
 // BCD arithmetic support
 #include "bcd.h"
 //#link "bcd.c"
@@ -32,8 +44,12 @@
 //#link "vrambuf.c"
 
 byte i;
-word counters[128];
-byte irqcount = 0;
+byte sine_xo; // origin point of screen
+byte sine_yo;
+byte sine_xc; // offset point for interupt
+byte sine_yc;
+byte next_x;
+byte next_y;
 
 
 /*{pal:"nes",layout:"nes"}*/
@@ -48,16 +64,46 @@ const char PALETTE[32] = {
   0x00,0x0d,0x27,0x2a
 };
 
+void __fastcall__ sine_pos_next(void) {
+    // advance to next scroll value
+    sine_xc += 11;
+    next_x = (sine[sine_xc] >> 2) - 52;
+    //if (sine_xc & 0x07) sine_yc++;
+    sine_yc += 7;
+    next_y = sine[sine_yc] >> 6;
+}
+
 void __fastcall__ irq_nmi_callback(void) {
   // check high bit of A to see if this is an IRQ
   if (__A__ & 0x80) {
     // it's an IRQ from the MMC3 mapper
     // change PPU scroll registers
-    PPU.scroll = counters[irqcount & 0x7f] >> 8;
-    PPU.scroll = 0;
-    scroll(counters[irqcount & 0x7f] >> 8, (counters[0x75 - irqcount] >> 8));
-    // advance to next scroll value
-    ++irqcount;
+//    scroll(sine[irq_count], sine[irq_count]);
+    //PPU.scroll = next_x;
+    //PPU.scroll = next_y;
+    //scroll(next_x, next_y);
+    //PEEK(PPU_STATUS);
+    POKE(PPU_ADDR, 0x20);
+    //POKE(PPU_ADDR, 0xc0);
+    POKE(PPU_SCROLL, next_x);
+    POKE(PPU_SCROLL, next_y);
+    /*
+	bit PPU_STATUS
+        lda scroll_x_hi
+        sta PPU_SCROLL
+        lda scroll_y
+        sta PPU_SCROLL
+        ; Vertical Scroll Solution
+        ;lda #$26
+        ;sta PPU_ADDR
+        ;lda #$c0
+        ;sta PPU_ADDR
+        ;lda #$00
+        ;sta PPU_SCROLL
+        ;sta PPU_SCROLL
+        */
+    
+    sine_pos_next();
     // acknowledge interrupt
     MMC3_IRQ_DISABLE();
     MMC3_IRQ_ENABLE();
@@ -66,11 +112,17 @@ void __fastcall__ irq_nmi_callback(void) {
     // reload IRQ counter
     MMC3_IRQ_RELOAD();
     // reset scroll counter
-    irqcount = 0;
+    sine_xo++;
+    sine_xc = sine_xo;
+    if (sine_xo & 0x01) sine_yo++;
+    sine_yc = sine_yo;
+    sine_pos_next();
   }
 }
 
 void main(void) {
+  sine_xo = 0;
+  sine_yo = 0x40;
   // clear sprites
   oam_clear();
   // set palette colors
@@ -85,7 +137,7 @@ void main(void) {
   // Mirroring - horizontal
   POKE(0xA000, 0x01);
   // set up MMC3 IRQs every 8 scanlines
-  MMC3_IRQ_SET_VALUE(9);
+  MMC3_IRQ_SET_VALUE(31);
   MMC3_IRQ_RELOAD();
   MMC3_IRQ_ENABLE();
   // enable CPU IRQ
@@ -110,10 +162,6 @@ void main(void) {
   ppu_on_all();
   // infinite loop
   while(1) {
-    byte i;
-    for (i=0; i<128; i++) {
-      counters[i] += i*16;
-    }
-    ppu_wait_frame();
+    //ppu_wait_frame();
   }
 }
